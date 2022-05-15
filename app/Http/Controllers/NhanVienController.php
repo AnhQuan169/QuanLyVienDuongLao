4572da3fdefcd5dabf40d0679f95305e654e5d5b
AnhQuan169 <anhquannguyen169@gmail.com>
2022-05-15 17:33:21 +0700

    Quản lý nhân viên: Xem chi tiết thông tin, cập nhật thông tin, cập nhật tình trạng nhân viên

--- app/Http/Controllers/NhanVienController.php
+++ app/Http/Controllers/NhanVienController.php
@@ -36,24 +36,90 @@ class NhanVienController extends Controller
 
     }
     // Hiển thị giao diện chỉnh sửa của nhân viên được chọn
-    public function edit_employee(){
-
+    public function edit_employee(Request $request,$id){
+        if(Gate::allows('quanly')) {
+            $employee = User::find($id);
+            $title = 'Chi tiết thông tin nhân viên';
+            $url = $request->url();
+            return view('admin.QuanLyTrungTam.QuanLyNhanVien.detail', compact('employee','title','url'));
+        }
+        return redirect()->back();
     }
 
     // Lưu thông tin cập nhật
-    public function update_employee(){
+    public function update_employee(Request $request, $id){
+        $data = array();
+        $data['email'] = $request->email;
+        $data['hoTen'] = $request->hoTen;
+        $data['loaiTaiKhoan'] = $request->loaiTaiKhoan;
+        $data['gioiTinh'] = $request->gioiTinh;
+        $data['ngaySinh'] = $request->ngaySinh;
+        $data['CCCD'] = $request->CCCD;
+        $data['soDienThoai'] = $request->soDienThoai;
+        $data['diaChi'] = $request->diaChi;
 
+        if(empty($data['email']) || empty($data['hoTen']) || empty($data['CCCD']) || empty($data['soDienThoai']) || empty($data['diaChi'])){
+            Toastr::warning('Vui lòng không để trống dữ liệu', 'Thất bại',);
+            return redirect()->back();
+        }else{
+            $employee = User::find($id);
+            if(User::where('email',$data['email'])->count() >0 && $data['email']!=$employee->email){
+                Toastr::warning('Địa chỉ email này đã tồn tại', 'Thất bại',);
+                return redirect()->back();
+            }else{
+                if(User::where('CCCD',$data['CCCD'])->count() >0 && $data['CCCD']!=$employee->CCCD){
+                    Toastr::warning('Số căn cước công dân này đã tồn tại', 'Thất bại',);
+                    return redirect()->back();
+                }else{
+                    if(User::where('soDienThoai',$data['soDienThoai'])->count() >0 && $data['soDienThoai']!=$employee->soDienThoai){
+                        Toastr::warning('Số điện thoại này đã tồn tại', 'Thất bại',);
+                        return redirect()->back();
+                    }else{
+                        $get_image = $request->file('anhDaiDien');
+                        if($get_image){
+                            $get_name_image = $get_image->getClientOriginalName();
+                            $name_image = current(explode('.',$get_name_image));
+                            $new_image = $name_image.rand(0,99).'.'.$get_image->getClientOriginalExtension();
+                            $get_image->move('public/admin/uploads/users',$new_image);
+                            $data['anhDaiDien'] = $new_image;
+                            User::where('id',$id)->update($data);
+                            Toastr::success('Cập nhật thông tin nhân viên thành công', 'Thành công',);
+                            return redirect()->back();
+                        }
+                        User::where('id',$id)->update($data);
+                        Toastr::success('Cập nhật thông tin nhân viên thành công', 'Thành công',);
+                        return redirect()->back();
+                    }
+                }
+            }
+        }
     }
     // Xoá nhân viên
     public function delete_employee(){
 
     }
     // Khoá tài khoản
-    public function unactive_employee(){
-
+    public function unactive_employee($id){
+        if(Gate::allows('quanly')) {
+            User::where('id',$id)
+            ->update([
+                'tinhTrang' => 2
+            ]);
+            Toastr::success('Khoá tài khoản', 'Thành công',);
+            return redirect()->back();
+        }
+        return redirect()->back();
     }
     // Khởi động tài khoản
-    public function active_employee(){
-
+    public function active_employee($id){
+        if(Gate::allows('quanly')) {
+            User::where('id',$id)
+            ->update([
+                'tinhTrang' => 1
+            ]);
+            Toastr::success('Khởi động tài khoản', 'Thành công',);
+            return redirect()->back();
+        }
+        return redirect()->back();
     }
 }
---
+++ resources/views/admin/QuanLyTrungTam/QuanLyNhanVien/detail.blade.php
@@ -0,0 +1,192 @@
+@extends('admin.admin_layout')
+@section('admin_content')
+<!--Start Page Title-->
+<div class="page-title-box">
+    <h4 class="page-title">{{$title}}</h4>
+    <ol class="breadcrumb">
+        <li>
+            <a href="{{route('dashboard')}}">Dashboard</a>
+        </li>
+        <li>
+            <a>Quản lý nhân viên</a>
+        </li>
+        <li class="active">
+            <a href="{{$url}}">{{$title}}</a>
+        </li>
+    </ol>
+    <div class="clearfix"></div>
+</div>
+<!--End Page Title-->       
+
+
+<!--Start row-->
+<div class="row">
+    <div class="col-md-12">
+        <div class="white-box">
+                
+                <form class="js-validation-bootstrap form-horizontal" action="{{route('employee.update',$employee->id)}}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <h4 class="text-center">Thông tin nhân viên</h4>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Họ tên nhân viên: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="hoTen" value="{{$employee->hoTen}}" class="form-control" type="text" placeholder="Nhập họ tên nhân viên">
+                            <span style="color: red;">
+                                @error('hoTen')
+                                {{$message}}
+                                @enderror
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Loại nhân viên: <span class="text-danger">*</span></label>
+                        <div class="col-md-9" style="display: flex">
+                            <select class="form-control" id="val-skill" name="loaiTaiKhoan">
+                                @if ($employee->loaiTaiKhoan == 1)
+                                    <option value="1">Nhân viên kho</option>
+                                    <option value="2">Nhân viên y tế</option>
+                                @else
+                                    <option value="2">Nhân viên y tế</option>
+                                    <option value="1">Nhân viên kho</option>
+                                @endif
+                                
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Email: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="email" value="{{$employee->email}}" class="form-control" type="text" placeholder="Nhập email">
+                            <span style="color: red;">
+                                @error('email')
+                                {{$message}}
+                                @enderror
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Giới tính: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <div style="margin-top: 5px;">
+                                <span>
+                                    <label><input name="gioiTinh" value="1" type="radio" {{$employee->gioiTinh==1?'checked':''}}> Nam</label>
+                                </span>
+                                <span style="margin-left: 20px">
+                                    <label><input name="gioiTinh" value="0" type="radio" {{$employee->gioiTinh==0?'checked':''}}> Nữ</label>
+                                </span>
+                            </div>
+                            <span style="color: red;">
+                                @error('gioiTinh')
+                                {{$message}}
+                                @enderror
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Ngày sinh: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="ngaySinh" value="{{$employee->ngaySinh}}" class="form-control" type="date">
+                            <span style="color: red;">
+                                @error('ngaySinh')
+                                {{$message}}
+                                @enderror
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Ảnh đại diện: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="anhDaiDien" class="form-control" value="{{$employee->anhDaiDien}}" type="file">
+                            <div id="large12" style="display: none;" class="cover-image-ncc">
+                                <img src="{{asset('public/admin/uploads/users/'.$employee->anhDaiDien)}}" alt="">
+                                <div id="close12" class="close-ncc">&times</div>
+                            </div>
+                            <img id="small12" class="small-nd" src="{{asset('public/admin/uploads/users/'.$employee->anhDaiDien)}}" />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Số căn cước công dân: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="CCCD" placeholder="Nhập số căn cước công dân" value="{{$employee->CCCD}}" class="form-control" type="text">
+                            <span style="color: red;">
+                                @error('CCCD')
+                                {{$message}}
+                                @enderror
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Số điện thoại: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="soDienThoai" placeholder="Nhập số điện thoại" value="{{$employee->soDienThoai}}" class="form-control" type="text">
+                            <span style="color: red;">
+                                @error('soDienThoai')
+                                {{$message}}
+                                @enderror
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Địa chỉ: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="diaChi" placeholder="Nhập địa chỉ" value="{{$employee->diaChi}}" class="form-control" type="text">
+                            <span style="color: red;">
+                                @error('diaChi')
+                                {{$message}}
+                                @enderror
+                            </span>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Tên tài khoản: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="name" value="{{$employee->name}}" class="form-control" type="text" disabled>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Ngày đăng ký: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="ngayDangKy"  value="{{$employee->ngayDangKy}}" class="form-control" type="date" disabled>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-md-3 control-label " for="val-username">Người thêm: <span class="text-danger">*</span></label>
+                        <div class="col-md-9">
+                            <input name="ngayDangKy"  value="{{$employee->nguoiDuyet}}" class="form-control" disabled>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-8 col-md-offset-3">
+                            <a href="{{route('employee.all')}}" class="btn  btn-instagram" type="button">Quay lại</a>
+                            <button class="btn btn-success save_employee" type="submit">Cập nhật</button>
+                        </div>
+                    </div>
+                </form>
+        </div>  
+    </div>
+</div>
+<!--End row-->
+@endsection
+
+@section('ajax_js')
+    <script type="text/javascript">
+        $(document).ready(function(){
+
+            
+
+            // Ảnh đại diện
+            $('#close12').click(function(){
+                $("#large12").hide();
+                $("#small12").show();
+            });
+
+            $('#small12').click(function(){
+                $("#large12").show();
+                $("#small12").hide();
+            });
+            
+            
+        });
+    </script>
+@endsection
